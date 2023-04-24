839427eb232880945a1327fcd243edffb33ff0db
ppom <>
2023-04-24 04:10:40 +0200

    Change program architecture for persistance
    Main will now only send matches to the database goroutine
    the database goroutine will handle the command execution logic
    this reduces a lot the interface between the 2 parts of the program.
    
    Main
    sends (Filter, pattern, time) to the database goroutine.
    all further logic has to be moved from main to database.

--- app/db.go
+++ app/db.go
@@ -18,81 +18,142 @@ func numberOfFilters(conf *Conf) int {
 	return n
 }
 
-type CmdTime struct {
-	cmd []string
-	t   time.Time
-}
-
-// Remove Cmd if last of its set
-type CmdExecuted struct {
-	filter  *Filter
-	pattern *string
-	value   CmdTime
-	err     chan error
-}
-
-// Append Cmd set
-type AppendCmd struct {
-	filter  *Filter
-	pattern *string
-	value   []CmdTime
-	err     chan error
-}
-
 // Append match, remove old ones and check match number
 type AppendMatch struct {
 	filter  *Filter
 	pattern *string
 	t       time.Time
-	ret     chan struct {
-		shouldExec bool
-		err        error
-	}
 }
 
 var env *lmdb.Env
 
+// e == "this match triggers an execution of the commands of the related filter"
+type MatchItem struct {
+	t time.Time
+	e bool
+}
+
+type PatternMatchItem struct {
+	pattern    string
+	matchItems []MatchItem
+}
+
 // keys: pattern
-// values: []time.Time
+// values: []struct { time.Time, bool triggerExec }
 var matchDBs map[*Filter]lmdb.DBI
 
-// keys: pattern
-// values: []CmdTime
-var cmdDBs map[*Filter]lmdb.DBI
-
-func dbGet(dbi lmdb.DBI, pattern string) (interface{}, error) {
+func dbGet(dbi lmdb.DBI, pattern string) ([]MatchItem, error) {
 	err = env.View(func(txn *lmdb.Txn) (err error) {
 		v, err := txn.Get(dbi, []byte(pattern))
 		if err != nil {
 			return nil, err
 		}
-		return json.Unmarshal(v)
+		value := make([]MatchItem)
+		err := json.Unmarshal(v, &value)
+		return value, err
 	})
 }
 
-func dbPut(dbi lmdb.DBI, pattern string, value interface{}) error {
+func dbPut(dbi lmdb.DBI, pattern string, value []MatchItem) error {
 	err = env.UpdateLocked(func(txn *lmdb.Txn) (err error) {
 		jsonValue := json.Marshal(value)
 		return txn.Put(dbi, []byte(pattern), jsonValue, 0)
 	})
 }
 
-func databaseHandler(chCE chan CmdExecuted, chAC chan AppendCmd, chAM chan AppendMatch) {
+func dbDel(dbi lmdb.DBI, pattern string) error {
+	err = env.UpdateLocked(func(txn *lmdb.Txn) (err error) {
+		return txn.Del(dbi, []byte(pattern), []byte())
+	})
+}
+
+func dbList(dbi lmdb.DBI) (chan []PatternMatchItem, chan error) {
+	items := make(chan []PatternMatchItem)
+	errors := make(chan []error)
+
+	go func() {
+
+		err = env.View(func(txn *lmdb.Txn) (err error) {
+			cur, err := txn.OpenCursor(dbi)
+			if err != nil {
+				return err
+			}
+			defer cur.Close()
+
+			for {
+				k, v, err := cur.Get(nil, nil, lmdb.Next)
+				if lmdb.IsNotFound(err) {
+					return nil
+				}
+				if err != nil {
+					return err
+				}
+
+				value := make([]MatchItem)
+				err = json.Unmarshal(v, &value)
+				if err != nil {
+					return err
+				}
+
+				items <- PatternMatchItem{
+					pattern:    string(k),
+					matchItems: value,
+				}
+			}
+		})
+	}()
+
+	return items, errors
+}
+
+// For now it's a list.
+// With a CLI, it might be useful to make a
+// map [*Filter]map[pattern]chan bool
+// if have a way to configure how reaction handles persistance,
+// bool would then mean if we execute or quit without executing
+var execActions []chan bool
+
+func databaseHandler(matches chan AppendMatch) {
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
 	defer env.Close()
+	defer func() {
+		for action := range execActions {
+			action <- true
+		}
+	}()
+
+	for filter, dbi := range matchDBs {
+		items, errs := dbList(dbi)
+		select {
+		case err := <-errs:
+			// warn user & delete entry if unmarshal failed?
+			// or panic, and tell user to delete db?
+		default:
+			select {
+			case item := <-items:
+				// check if last exec is not too old (now < lastExec + longuestCommand"After")
+				if true {
+					for command := range filter.commands {
+						// launch command
+					}
+				}
+			default:
+			}
+		}
+	}
 
 	select {
-	case ce := <-chCE:
-		// TODO
-		ce = ce
-	case ac := <-chAC:
-		err = env.UpdateLocked(func(txn *lmdb.Txn) (err error) {
-			return txn.Put(dbi, []byte("k"), []byte("v"), 0)
-		})
 	case am := <-chAM:
-		// TODO
-		am = am
+		currentMatches := dbGet(matchDBs[am.filter], am.pattern)
+		// add match
+		// check if enough watches in interval
+		if true {
+			for command := range am.filter.commands {
+				// launch command
+			}
+		}
+
 	}
 }
 
@@ -109,25 +170,19 @@ func initDatabase(conf *Conf) (chan CmdExecuted, chan AppendCmd, chan AppendMatc
 
 	filterNumber := numberOfFilters(conf)
 
-	err = env.SetMaxDBs(filterNumber * 2)
+	err = env.SetMaxDBs(filterNumber)
 	if err != nil {
 		log.Fatalln("LMDB.SetMaxDBs failed")
 	}
 
 	matchDBs = make(map[*Filter]lmdb.DBI, filterNumber)
-	cmdDBs = make(map[*Filter]lmdb.DBI, filterNumber)
 
 	runtime.LockOSThread()
 
 	for _, stream := range conf.Streams {
 		for _, filter := range stream.Filters {
 			err = env.UpdateLocked(func(txn *lmdb.Txn) (err error) {
-				matchDBs[filter], err = txn.CreateDBI(fmt.Sprintln("%s.%s.match", stream.name, filter.name))
-				if err != nil {
-					return err
-				}
-
-				cmdDBs[filter], err = txn.CreateDBI(fmt.Sprintln("%s.%s.cmd", stream.name, filter.name))
+				matchDBs[filter], err = txn.CreateDBI(fmt.Sprintln("%s.%s", stream.name, filter.name))
 				return err
 			})
 			if err != nil {
@@ -138,11 +193,9 @@ func initDatabase(conf *Conf) (chan CmdExecuted, chan AppendCmd, chan AppendMatc
 
 	runtime.UnlockOSThread()
 
-	chCE := make(chan CmdExecuted)
-	chAC := make(chan AppendCmd)
 	chAM := make(chan AppendMatch)
 
-	go databaseHandler(chCE, chAC, chAM)
+	go databaseHandler(chAM)
 
-	return chCE, chAC, chAM
+	return chAM
 }
